c17df53a4ca770697c6e81d488d75263750b8f9e
Vishaal Diwan <vishaaldiwan@Vishaals-MacBook-Pro.local>
2019-06-17 20:35:17 -0700

    validations complete

--- public/js/postevent.js
+++ public/js/postevent.js
@@ -5,61 +5,64 @@
 (function() {
   // Run JS once the document is ready
   $(document).ready(function() {
-    // When Register button is clicked
-    $("#event-submit").on("click", function() {
-      event.preventDefault();
+    $("#eventForm").validate({
+      submitHandler: function() {
+        // Create newEvent object with input values
+        var newEvent = {
+          orgId: $("#orgName").val(),
+          eventTitle: $("#eventTitle")
+            .val()
+            .trim(),
+          type: $("#type").val(),
+          date: $("#eventDate")
+            .val()
+            .trim(),
+          startTime: $("#startTime")
+            .val()
+            .trim(),
+          endTime: $("#endTime")
+            .val()
+            .trim(),
+          venueName: $("#placeTitle")
+            .val()
+            .trim(),
+          address: $("#eventInputAddress")
+            .val()
+            .trim(),
+          address2: $("#eventInputAddress2")
+            .val()
+            .trim(),
+          city: $("#eventInputCity")
+            .val()
+            .trim(),
+          state: $("#eventInputState")
+            .val()
+            .trim(),
+          zip: $("#eventInputZip").val(),
+          description: $("#eventDescription")
+            .val()
+            .trim()
+        };
 
-      // Create newEvent object with input values
-      var newEvent = {
-        orgId: $("#orgName").val(),
-        eventTitle: $("#eventTitle")
-          .val()
-          .trim(),
-        type: $("#type").val(),
-        date: $("#eventDate")
-          .val()
-          .trim(),
-        startTime: $("#startTime")
-          .val()
-          .trim(),
-        endTime: $("#endTime")
-          .val()
-          .trim(),
-        venueName: $("#placeTitle")
-          .val()
-          .trim(),
-        address: $("#eventInputAddress")
-          .val()
-          .trim(),
-        address2: $("#eventInputAddress2")
-          .val()
-          .trim(),
-        city: $("#eventInputCity")
-          .val()
-          .trim(),
-        state: $("#eventInputState")
-          .val()
-          .trim(),
-        zip: $("#eventInputZip").val(),
-        description: $("#eventDescription")
-          .val()
-          .trim()
-      };
+        console.log(newEvent);
 
-      console.log(newEvent);
-
-      // Add new event to Events table in database
-      $.ajax({
-        headers: {
-          "Content-Type": "application/json"
-        },
-        type: "POST",
-        url: "api/events",
-        data: JSON.stringify(newEvent)
-      }).then(function() {
-        // Redirect to dashboard once event is added
-        window.location.replace("/dashboard");
-      });
+        // Add new event to Events table in database
+        $.ajax({
+          headers: {
+            "Content-Type": "application/json"
+          },
+          type: "POST",
+          url: "api/events",
+          data: JSON.stringify(newEvent)
+        }).then(function() {
+          // Redirect to dashboard once event is added
+          window.location.replace("/dashboard");
+        });
+      }
     });
+    // When Register button is clicked
+    // $("#event-submit").on("click", function() {
+    //   event.preventDefault();
+    // });
   });
 })();
